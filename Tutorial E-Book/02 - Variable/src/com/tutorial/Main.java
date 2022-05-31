6980f29314a930be88570801a9b72c98285ab892
Maulanawesome5 <maulanawesome5@gmail.com>
2022-05-31 22:31:14 +0700

    E-Book Tutorial Java : Variable

---
+++ Tutorial E-Book/02 - Variable/src/com/tutorial/Main.java
@@ -0,0 +1,37 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+        /*
+         * Tutorial ini saya peroleh melalui e-book pdf.
+         * Tidak ada bagian cover, dan daftar isi
+         * cuma berjudul Pemrograman Java disertai daftar pustaka di akhir halaman.
+         * Yang ditulis oleh seorang bernama Imam Muiz,S.Kom.
+         * Referensi penulis e-book berasal dari buku/jurnal luar negeri
+         * yang terbit sekitaran tahun 2000 - 2008.
+         */
+
+        int var1 = 70;
+        int var2;
+        int val;
+
+        var2 = 30;
+
+        val = var1 + var2;
+        System.out.println(var1 + " + " + var2 + " = " + val);
+
+        val = var1 - var2;
+        System.out.println(var1 + " - " + var2 + " = " + val);
+
+        val = var1 * var2;
+        System.out.println(var1 + " * " + var2 + " = " + val);
+
+        val = var1 / var2;
+        System.out.println(var1 + " / " + var2 + " = " + val);
+
+        val = var1 % var2;
+        System.out.println(var1 + " % " + var2 + " = " + val);
+    }
+
+}
