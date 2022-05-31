3fbf9637ba5d1ba8f7683ad0b7cc47fccdd44b69
Maulanawesome5 <maulanawesome5@gmail.com>
2022-05-31 22:30:56 +0700

    E-Book Tutorial Java : Print Console

---
+++ Tutorial E-Book/01 - Print Console/src/com/tutorial/Main.java
@@ -0,0 +1,20 @@
+package com.tutorial;
+
+public class Main {
+
+    public static void main(String[] args){
+        /*
+         * Tutorial ini saya peroleh melalui e-book pdf.
+         * Tidak ada bagian cover, dan daftar isi
+         * cuma berjudul Pemrograman Java disertai daftar pustaka di akhir halaman.
+         * Yang ditulis oleh seorang bernama Imam Muiz,S.Kom.
+         * Referensi penulis e-book berasal dari buku/jurnal luar negeri
+         * yang terbit sekitaran tahun 2000 - 2008.
+        */
+
+        System.out.println("Hello World !");
+        System.out.println("E-book Tutorial Pemrograman Java");
+        System.out.println("oleh Imam Muiz, S.Kom.");
+    }
+
+}
